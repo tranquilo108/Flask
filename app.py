7d5e5df7acd36c6265a34080f752618931a65dc6
tranquilo <sanbonzakurakogyoshi@gmail.com>
2024-02-16 09:28:30 +0700

    HW2

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
+import secrets
 from flask import Flask, render_template, request, redirect, url_for, flash, make_response
 
+KEY = secrets.token_hex(4)
 app = Flask(__name__)
-app.secret_key = 'your_secret_key'  # Замените 'your_secret_key' на ваш секретный ключ
+app.secret_key = KEY
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -10,7 +12,6 @@ def index():
         name = request.form.get('name')
         email = request.form.get('email')
 
-        # Создаем cookie файл
         response = make_response(redirect(url_for('greet')))
         response.set_cookie('user_name', name)
         response.set_cookie('user_email', email)
@@ -22,7 +23,6 @@ def index():
 
 @app.route('/greet')
 def greet():
-    # Получаем данные из cookie
     user_name = request.cookies.get('user_name')
     user_email = request.cookies.get('user_email')
 
@@ -31,7 +31,6 @@ def greet():
 
 @app.route('/logout')
 def logout():
-    # Удаляем cookie файлы
     response = make_response(redirect(url_for('index')))
     response.delete_cookie('user_name')
     response.delete_cookie('user_email')
